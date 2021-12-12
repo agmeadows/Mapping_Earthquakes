e44034e4650e2dc703e5c394cebffb34160d565f
agmeadows <65190360+agmeadows@users.noreply.github.com>
2021-12-12 00:40:00 -0500

    update files

--- Simple_map/static/js/logic.js
+++ Simple_map/static/js/logic.js
@@ -10,10 +10,10 @@ let map = L.map("mapid", {
 // Adding tile layer
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+  tileSize: 512,
   maxZoom: 18,
   zoomOffset: -1,
   id: "mapbox/streets-v11",
-  tilesize: 512,
   accessToken: API_KEY
 });
 // Add tile layer to map
