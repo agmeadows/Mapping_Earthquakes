01febfb379e6c809eaaef3482f7813d60af894df
agmeadows <65190360+agmeadows@users.noreply.github.com>
2021-12-12 00:43:29 -0500

    Update logic.js

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -19,3 +19,9 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 // Add tile layer to map
 streets.addTo(map);
 
+//  Add a marker to the map for Los Angeles, California.
+let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+L.circle([34.0522, -118.2437], {
+  radius: 100
+}).addTo(map);
